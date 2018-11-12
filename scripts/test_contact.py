d1b8b37d5e172b4799dbd8ba9f88443f6efb704c
LAPTOP-1C64MBNA\15101 <wlf15101641959@163.com>
2018-11-12 17:29:21 +0800

    1

---
+++ scripts/test_contact.py
@@ -0,0 +1,38 @@
+import time
+import pytest
+
+from base.base_driver import init_driver
+from page.page import Page
+from base.base_analyze import analyze_file
+
+
+class TestContact:
+
+    def test_01(self):
+        assert 0
+
+    def test_02(self):
+        assert 1
+
+    def test_03(self):
+        assert 0
+    # def setup(self):
+    #     self.driver = init_driver()
+    #     self.page = Page(self.driver)
+    #
+    # def teardown(self):
+    #     time.sleep(5)
+    #     self.driver.quit()
+    #
+    # @pytest.mark.parametrize("args", analyze_file("contact_data.yml", "test_add_contact"))
+    # def test_add_contact(self, args):
+    #     name = args["name"]
+    #     phone = args["phone"]
+    #
+    #     self.page.contact.click_add_contact()
+    #     self.page.new_contact.input_name(name)
+    #     self.page.new_contact.input_phone(phone)
+    #     self.page.new_contact.click_back()
+    #     assert name == self.page.saved_contect.get_name_title_text()
+
+
